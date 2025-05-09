c172a24c5acf9148653ab3383b19cbd18ee11bd1
Arthur Rios Ribeiro <arthurriosribeiro@gmail.com>
2025-05-09 17:51:41 -0300

    Ajustes nas seções

--- src/js/contants.js
+++ src/js/contants.js
@@ -9,12 +9,12 @@ const  Constants = {
         {
             title: "Como o celular afeta sua ansiedade",
             text: "<p>Use este espaço para inserir um trecho ou uma pré-visualização do seu texto principal. Você pode simplesmente adicionar o primeiro parágrafo ou criar um resumo.</p><p>Use este espaço para inserir um trecho ou uma pré-visualização do seu texto principal. Você pode simplesmente adicionar o primeiro parágrafo ou criar um resumo.</p>",
-            img: "../assets/images/fernanda_laptop_lg.png"
+            img: "assets/images/fernanda_laptop_lg.png"
         },
         {
             title: "De que forma você tem lidado com seus erros?",
             text: "<p>Use este espaço para inserir um trecho ou uma pré-visualização do seu texto principal. Você pode simplesmente adicionar o primeiro parágrafo ou criar um resumo.</p><p>Use este espaço para inserir um trecho ou uma pré-visualização do seu texto principal. Você pode simplesmente adicionar o primeiro parágrafo ou criar um resumo.</p>",
-            img: "../assets/images/broken_cup.jpg"
+            img: "assets/images/broken_cup.jpg"
         }
     ]
 }
